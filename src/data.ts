bd2b20a56331ee72349869a8c4dabda2f9fd1516
shaaibu <shaaibusuleiman@gamil.com>
2023-09-11 08:56:26 +0100

    simulate local database

---
+++ src/data.ts
@@ -0,0 +1,19 @@
+export const data: Data = {
+  report: [],
+};
+
+interface Data {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: ReportType;
+  }[];
+}
+
+enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
