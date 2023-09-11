3be9b452d1c8ae5316b844b10b037ad9a3750ba2
shaaibu <shaaibusuleiman@gamil.com>
2023-09-11 08:31:48 +0100

    add endpoint in controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
-@Controller()
+@Controller('report/:type')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllReports() {
+    return [];
+  }
+
+  @Get(':id')
+  getAllReportsById() {
+    return {};
+  }
+
+  @Post()
+  createReport() {
+    return 'Ressource created successfully';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'updated';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'Report deleted';
   }
 }
