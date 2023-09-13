bdd82bc4d0bc799b4767ad84002d196cf3eea22d
shaaibu <shaaibusuleiman@gamil.com>
2023-09-13 08:15:35 +0100

    add report and summary resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,17 @@ import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { CustomInterceptor } from './custom.interceptor';
+import { SummaryModule } from './summary/summary.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [],
+  imports: [SummaryModule, ReportModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: APP_INTERCEPTOR,
-      useClass: CustomInterceptor,
+      useClass: ClassSerializerInterceptor,
     },
   ],
 })
